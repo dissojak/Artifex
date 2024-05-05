150e3b6ed2320ea7f096f5f81989bf425b6b6d44
Adem Ben Amor <dissojak@gmail.com>
2024-05-05 23:32:51 +0100

    adem : nav barre working !!!!!!

--- frontend/src/shared/components/Nav/NavArtifex.jsx
+++ frontend/src/shared/components/Nav/NavArtifex.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import "./NavArtifex.css";
 import SaveIcon from "../../../assets/images/saveblack.svg";
 import SaveIconActive from "../../../assets/images/savepurple.svg";
@@ -6,113 +7,91 @@ import PinIcon from "../../../assets/images/Pinblack.svg";
 import PinIconActive from "../../../assets/images/Pinpurple.svg";
 import OrderIcon from "../../../assets/images/orderblack.svg";
 import OrderIconActive from "../../../assets/images/orderpurple.svg";
-import { AuthContext } from "../../context/auth-context";
-import { NavLink } from "react-router-dom";
 
 const NavArtifex = () => {
-  const [activeTab, setActiveTab] = useState("home");
-  const auth = useContext(AuthContext);
+  const location = useLocation();
+  const homeRef = useRef(null);
+  const artistsRef = useRef(null);
+  const profileRef = useRef(null);
+  const indicatorRef = useRef(null);
 
-  useEffect(() => {
-    const handleRouteChange = () => {
-      // Set the active tab based on the current path
-      const path = window.location.pathname;
-      console.log(path);
-      if (path === "/") setActiveTab("home");
-      else if (path.includes("artists")) setActiveTab("artists");
-      else if (path.includes("profile")) setActiveTab("profile");
-    };
-  
-    window.addEventListener('popstate', handleRouteChange);
-  
-    return () => window.removeEventListener('popstate', handleRouteChange);
-  }, []);
-
-  const getIcon = (tabName) => {
-    if (tabName === "home") {
-      return activeTab === "home" ? OrderIconActive : OrderIcon;
-    } else if (tabName === "artists") {
-      return activeTab === "artists" ? SaveIconActive : SaveIcon;
-    } else if (tabName === "profile") {
-      return activeTab === "profile" ? PinIconActive : PinIcon;
+  const getIcon = (path) => {
+    switch (path) {
+      case "/":
+        return location.pathname === "/" ? OrderIconActive : OrderIcon;
+      case "/artists":
+        return location.pathname.includes("artists")
+          ? SaveIconActive
+          : SaveIcon;
+      case "/profile":
+        return location.pathname.includes("profile") ? PinIconActive : PinIcon;
+      default:
+        return OrderIcon;
     }
   };
 
+  useEffect(() => {
+    let activeRef;
+    if (location.pathname === "/") {
+      activeRef = homeRef;
+    } else if (location.pathname.includes("artists")) {
+      activeRef = artistsRef;
+    } else if (location.pathname.includes("profile")) {
+      activeRef = profileRef;
+    }
+
+    if (activeRef.current && indicatorRef.current) {
+      const { offsetLeft, clientWidth } = activeRef.current;
+      indicatorRef.current.style.width = `${clientWidth}px`;
+      indicatorRef.current.style.left = `${offsetLeft}px`;
+      indicatorRef.current.style.transition = "left 0.2s ease, width 0.2s ease";
+    }
+  }, [location]);
+
   return (
-    <>
-      <div className="Buttons-section31">
-        <div className="tab-container10">
-          <NavLink to="/" exact="true" className="text-decoration-link">
-            <input
-              type="radio"
-              name="tabNav"
-              id="tab11"
-              className="tab1 tab1--1"
-              checked={activeTab === "home"}
-              onChange={() => setActiveTab("home")}
-            />
-            <label
-              className={`tab1_label ${activeTab === "home" ? "active" : ""}`}
-              htmlFor="tab1"
-            >
-              <img
-                src={getIcon("home")}
-                style={{ width: "1.2em", height: "1.2em" }}
-                alt="Order Icon"
-              />
-              <p className="testeeee">Home</p>
-            </label>
-          </NavLink>
-          <NavLink to="/artists" exact="true" className="text-decoration-link">
-            <input
-              type="radio"
-              name="tabNav"
-              id="tab22"
-              className="tab1 tab1--2"
-              checked={activeTab === "artists"}
-              onChange={() => setActiveTab("artists")}
-            />
-            <label
-              className={`tab1_label ${
-                activeTab === "artists" ? "active" : ""
-              }`}
-              htmlFor="tab2"
-            >
-              <img
-                src={getIcon("artists")}
-                style={{ width: "1.2em", height: "1.2em" }}
-                alt="Save Icon"
-              />
-              Artists
-            </label>
-          </NavLink>
-          <NavLink to="/profile" exact="true" className="text-decoration-link">
-            <input
-              type="radio"
-              name="tabNav"
-              id="tab33"
-              className="tab1 tab1--3"
-              checked={activeTab === "profile"}
-              onChange={() => setActiveTab("profile")}
-            />
-            <label
-              className={`tab1_label ${
-                activeTab === "profile" ? "active" : ""
-              }`}
-              htmlFor="tab3"
-            >
-              <img
-                src={getIcon("profile")}
-                style={{ width: "1.2em", height: "1.2em" }}
-                alt="Pin Icon"
-              />
-              Profile
-            </label>
-          </NavLink>
-          <div className="indicator10"></div>
-        </div>
+    <div className="Buttons-section31">
+      <div className="tab-container10">
+        <NavLink
+          to="/"
+          exact
+          activeClassName="active"
+          className="tab1_label text-decoration-link"
+          ref={homeRef}
+        >
+          <img src={getIcon("/")} alt="Home Icon" style={{ marginRight: 5 }} />
+          Home
+        </NavLink>
+        <NavLink
+          to="/artists"
+          exact
+          activeClassName="active"
+          className="tab1_label text-decoration-link"
+          ref={artistsRef}
+        >
+          <img
+            src={getIcon("/artists")}
+            alt="Artists Icon"
+            style={{ marginRight: 5 }}
+          />
+          Artists
+        </NavLink>
+        <NavLink
+          to="/profile"
+          exact
+          activeClassName="active"
+          className="tab1_label text-decoration-link"
+          ref={profileRef}
+        >
+          <img
+            src={getIcon("/profile")}
+            alt="Profile Icon"
+            style={{ marginRight: 5 }}
+          />
+          Profile
+        </NavLink>
+        <div className="indicator10" ref={indicatorRef}></div>
       </div>
-    </>
+    </div>
   );
 };
 
