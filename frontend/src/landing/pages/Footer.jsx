5043ffaafe3f963476bcbbb9942ec2208b48feb5
Tarek <tarekchebbi33@gmail.com>
2024-05-08 00:32:07 +0100

    Tarek : footer Update

--- frontend/src/landing/pages/Footer.jsx
+++ frontend/src/landing/pages/Footer.jsx
@@ -12,46 +12,85 @@ import Mail from '../../assets/images/mail.png';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <Link to="/" className="footer-logo">
-        <img src={Logo} alt="Artifex Logo" />
-      </Link>
-      <div className="footer-info">
-        <p><img src={Location} alt="location" />Address: Nabeul, Nabeul Center 8000 Tunisia</p>
-        <p><img src={Telephone} alt="Telephone" /> Call Us: (+50) - 52-025-123</p>
-        <p><img src={Mail} alt="Mail" /> Email: stoonproduction@gmail.com, tarekchebbi33@gmail.com</p>
+        <footer>
+      <div className="background">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="100%"
+          height="100%"
+          viewBox="0 0 1600 900"
+        >
+          <defs>
+            <path
+              id="wave"
+              fill="rgba(105, 27, 252, 0.6)"
+              d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+                s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
+            />
+          </defs>
+          <g>
+            <use xlinkHref="#wave" opacity=".4">
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="8s"
+                calcMode="spline"
+                values="270 230; -334 180; 270 230"
+                keyTimes="0; .5; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
+              />
+            </use>
+            <use xlinkHref="#wave" opacity=".6">
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="6s"
+                calcMode="spline"
+                values="-270 230;243 220;-270 230"
+                keyTimes="0; .6; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
+              />
+            </use>
+            <use xlinkHref="#wave" opacity=".9">
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="4s"
+                calcMode="spline"
+                values="0 230;-140 200;0 230"
+                keyTimes="0; .4; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
+              />
+            </use>
+          </g>
+        </svg>
       </div>
-      <div className="footer-info">
-      <div className="footer-section">
-    <h3>Popular</h3>
-    <ul>
-      <li><Link to="/about">About Us</Link></li>
-      <li><Link to="/artists">Artists</Link></li>
-      <li><Link to="/faq">FAQ</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
-      <li><Link to="/support">Support</Link></li>
-    </ul>
-  </div>
-  </div>
-      <div className="footer-info">
-      <div className="footer-section">
-    <h3>Corporate</h3>
-    <ul>
-      <li><Link to="/privacy">Privacy Policy</Link></li>
-      <li><Link to="/delivery">Delivery Information</Link></li>
-      <li><Link to="/terms">Terms & Conditions</Link></li>
-      <li><Link to="/cookies">Cookies Settings</Link></li>
-    </ul>
-  </div>
-      </div>
-      <div className="footer-links">
-        <h3>Follow Us</h3>
-        <div className="social-links">
-          <Link to="#">  <img src={Facebook} alt="Facebook" /></Link>
-          <Link to="#">  <img src={Instagram} alt="Instagram" /></Link>
-          <Link to="#">  <img src={Twitter} alt="FaceTwitterbook" /></Link>
-          <Link to="#">  <img src={Youtube} alt="Youtube" /></Link>
-        </div>
-      </div>
+      <section>
+        <ul className="socials">
+          <li><Link to=""><img src={Facebook} style={{width:'1em',height:'1em'}} alt='facebook' /></Link></li>
+          <li><Link to=""><img src={Instagram} style={{width:'1em',height:'1em'}} alt='Instagram' /></Link></li>
+          <li><Link to=""><img src={Twitter} style={{width:'1em',height:'1em'}} alt='Twitter' /></Link></li>
+        </ul>
+        <ul className="links">
+          <li>About Us</li>
+          <li>Artists</li>
+          <li>FAQ</li>
+          <li>Contact</li>
+          <li>Support</li>
+        </ul>
+        <p className="legal">© 2024 Artifex All rights reserved</p>
+      </section>
+    </footer>
     </div>
   );
 };
