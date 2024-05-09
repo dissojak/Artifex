4615364fe391cdf4f883ddcbfc4ca9106afbc480
Adem Ben Amor <dissojak@gmail.com>
2024-05-09 03:33:08 +0100

    adem : home background bug fix

--- frontend/src/home/Components/Arts.jsx
+++ frontend/src/home/Components/Arts.jsx
@@ -96,27 +96,29 @@ const Artworks = [
     price: "12",
   },
 ];
-const isLoading =false;
+const isLoading = false;
 const Arts = () => {
   //   document.documentElement.style.setProperty('--scrollbar-thumb-color', !auth.isAdmin ? '#87CEEB' : '#C99C6E');
 
   return (
     <Fragment>
       <br />
-      {/* <div id="homearts"> */}
-      <img src="./elements/line.svg" className="backgroundLineArts" alt="" />
-        <div className="auth-section2">
-          <h1 style={{ fontWeight: "bold", fontSize: "40px" }}>Art Showcase</h1>
-          <p>Discover Exquisite Artworks from Talented Artist</p>
+      <div id="HomeContainerClinet">
+      {/* <div className="backgroundLineArtsContainer">
+        <img src="./elements/line.svg" className="backgroundLineArts" alt="" />
+      </div> */}
+      <div className="auth-section2">
+        <h1 style={{ fontWeight: "bold", fontSize: "40px" }}>Art Showcase</h1>
+        <p>Discover Exquisite Artworks from Talented Artist</p>
+      </div>
+      {isLoading && (
+        <div className="center_spinner">
+          {/* <LoadingSpinner /> */}
+          <img src="./elements/11a.gif" alt="" />
         </div>
-        {isLoading && (
-            <div className="center_spinner">
-              {/* <LoadingSpinner /> */}
-              <img src="./elements/11a.gif" alt="" />
-            </div>
-          )}
-          {!isLoading && Artworks && <ArtsList items={Artworks}/>}
-      {/* </div> */}
+      )}
+      {!isLoading && Artworks && <ArtsList items={Artworks} />}
+      </div>
     </Fragment>
   );
 };
