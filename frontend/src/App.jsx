d1c776bf146f5ad4abed2fbc0725b34680fe5179
Tarek <tarekchebbi33@gmail.com>
2024-05-09 17:24:08 +0100

    Tarek : ArtistProfile For User update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,6 +36,7 @@ import Museums from "./MuseumPage/Pages/MuseumPage.jsx";
 import UserProfile from "./UserProfile/Pages/UserProfile.jsx";
 import SocialMedia from "./user/pages/SocialMedia.jsx";
 import ArtworkDetails from "./ArtworkDetails/Pages/ArtworkDetails.jsx";
+import ArtistProfileForUser from "./ArtistProfileForUser/Pages/ArtistProfileForUser.jsx";
 import NavigationWrapper from "./shared/components/Navigation/NavigationWrapper.jsx";
 
 const App = () => {
@@ -120,6 +121,7 @@ const App = () => {
           <Route path="/museums" element={<Museums />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/ArtworkDetails" element={<ArtworkDetails />} />
+          <Route path="/artistprofile" element={<ArtistProfileForUser />} />
         </Route>
         <Route path="/login" element={<Auth />} />
         {/* <Route path="/auth" element={<Auth />} /> */}
--- frontend/src/ArtistProfileForUser/Pages/ArtistProfileForUser.jsx
+++ frontend/src/ArtistProfileForUser/Pages/ArtistProfileForUser.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './ArtistProfileForUser.css';
-
+import ProfileSection from "../Components/ProfileSection.jsx";
 
 const ArtistProfileForUser = () => {
   return (
     <div className="ArtistProfileForUser-container">
    
-   
+   <ProfileSection />
       
     </div>
   );
