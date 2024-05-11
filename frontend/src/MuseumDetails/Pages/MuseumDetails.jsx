998a23cef2f4d1c97a499f26eaf41b2db5defdd4
Adem Ben Amor <dissojak@gmail.com>
2024-05-11 20:50:07 +0100

    adem : ticket pass style

--- frontend/src/MuseumDetails/Pages/MuseumDetails.jsx
+++ frontend/src/MuseumDetails/Pages/MuseumDetails.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./MuseumDetails.css";
 import ArtSection from "../Components/ArtSection.jsx";
 import ArtistParticipitant from "../Components/ArtistParticipitant.jsx";
 const MuseumDetails = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="MuseumDetails-container">
-<ArtSection />
-<ArtistParticipitant />
+      <ArtSection />
+      <ArtistParticipitant />
     </div>
   );
 };
