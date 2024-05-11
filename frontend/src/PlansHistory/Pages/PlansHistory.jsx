a3db776a848787e106a6ecd7ccd8322d8bcec306
Tarek <tarekchebbi33@gmail.com>
2024-05-11 20:26:13 +0100

    changes

---
+++ frontend/src/PlansHistory/Pages/PlansHistory.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './PlansHistory.css';
+import PlansHistory from "../Components/PlansHistorySection.jsx";
+const PlansHistory = () => {
+  return (
+    <div className="PlansHistory-container">
+
+      
+    </div>
+  );
+};
+
+export default PlansHistory;
