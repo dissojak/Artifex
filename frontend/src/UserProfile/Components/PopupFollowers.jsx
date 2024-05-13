ae3e80538ad337f7c08fcb05ed14bc5018fe5b3c
Tarek <tarekchebbi33@gmail.com>
2024-05-13 15:26:26 +0100

    changes 3

--- frontend/src/UserProfile/Components/PopupFollowers.jsx
+++ frontend/src/UserProfile/Components/PopupFollowers.jsx
@@ -3,23 +3,41 @@ import React, { useState } from "react";
 import "./PopupFollowers.css";
 import Tarek from "../../assets/images/tarek.png";
 import Unfollow from "../../assets/images/unfollow.svg";
+const Followers = [
+    { Image: Tarek, Artist: "Tarek Chebbi", Category: "Digital Art"},
+    { Image: Tarek, Artist: "Tarek Chebbi", Category: "Digital Art"},
+    { Image: Tarek, Artist: "Tarek Chebbi", Category: "Digital Art"},
+    { Image: Tarek, Artist: "Tarek Chebbi", Category: "Digital Art"},
+  ];
 const PopupFollowers = ({ onClose }) => {
 
   return (
     <>
       <div className="modal-content">
-      <h2>Following</h2>
+        
+      <p style={{fontSize:'24px',fontFamily:'Raleway-Bold'}}>Following  <img
+              src="elements/X.svg"
+              alt=""
+              className="tit_ch_img_in_followers"
+              onClick={onClose}
+            /></p>
+     
       <div className="followers-list">
+      {Followers.map((Follower) => (
         <div className="follower-item">
-          <img src={Tarek} alt="avatar" />
+        <div className="follower-img-avatar">
+          <img src={Follower.Image} alt="avatar" />
+          </div>
           <div className="follower-info">
-              <span className="follower-name">Hmed S'atour</span>
-              <span className="follower-title">Digital Artist</span>
+              <span className="follower-name">{Follower.Artist}</span>
+              <span className="follower-title">{Follower.Category}</span>
             </div>
-          <span className="status-icon"><img src={Unfollow} style={{width:'15px',height:'15px'}} alt="unfollow icon" /></span>
+          <span className="status-icon"><img src={Unfollow} style={{width:'35px',height:'35px'}} alt="unfollow icon" /></span>
         </div>
+           ))}
       </div>
-      <button onClick={onClose}>Close</button>
+     
+     
     </div>
     </>
   );
