a2d44846d5ccc5233176969c6b69ae9f8e3c49de
Adem Ben Amor <dissojak@gmail.com>
2024-05-13 11:31:53 +0100

    adem : museum button on usertype

--- frontend/src/MuseumPage/Components/Introduction.jsx
+++ frontend/src/MuseumPage/Components/Introduction.jsx
@@ -5,15 +5,15 @@ import "./Introduction.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../shared/context/auth-context";
 
-import Facebook from '../../assets/images/Facebook.svg';
-import Instagram from '../../assets/images/Instagram.svg';
-import Twitter from '../../assets/images/Tracé 2.svg';
-import Youtube from '../../assets/images/youtube.svg';
-import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { useLogoutMutation } from '../../slices/usersApiSlice';
-import { logout } from '../../slices/authSlice';
-import Logo from '../../assets/images/Logo_Artifex.svg';
+import Facebook from "../../assets/images/Facebook.svg";
+import Instagram from "../../assets/images/Instagram.svg";
+import Twitter from "../../assets/images/Tracé 2.svg";
+import Youtube from "../../assets/images/youtube.svg";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../../slices/usersApiSlice";
+import { logout } from "../../slices/authSlice";
+import Logo from "../../assets/images/Logo_Artifex.svg";
 
 const Introduction = () => {
   const auth = useContext(AuthContext);
@@ -28,19 +28,16 @@ const Introduction = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
-      navigate('/login');
+      navigate("/login");
     } catch (err) {
       console.error(err);
     }
   };
 
-
   //   document.documentElement.style.setProperty('--scrollbar-thumb-color', !auth.isAdmin ? '#87CEEB' : '#C99C6E');
 
   return (
     <>
-     
-      
       {auth.isLoggedIn && (
         <>
           <div id="header_right_home">
@@ -59,156 +56,201 @@ const Introduction = () => {
             className="where"
             style={!auth.isAdmin ? { color: "#5BD6FF" } : { color: "#5BD6FF" }}
           >
-             Where Every Brushstroke{" "}
+            Where Every Brushstroke{" "}
           </h1>
           <h1 className="meets">Tells a Story</h1>
           <p className="p1">
-            "Explore the Rich Tapestry of Human Creativity. At Artifex Museums, we invite you to embark on a journey through time,
- where every exhibit unveils new dimensions of imagination and unlocks the boundless potential of human expression."
+            "Explore the Rich Tapestry of Human Creativity. At Artifex Museums,
+            we invite you to embark on a journey through time, where every
+            exhibit unveils new dimensions of imagination and unlocks the
+            boundless potential of human expression."
           </p>
-          <Link to="/Plans">
-          <button className="magic-button">
-  Get Your Plan
-  <div className="star-1">
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  style={{
-    shapeRendering: "geometricPrecision",
-    textRendering: "geometricPrecision",
-    imageRendering: "optimizeQuality",
-    fillRule: "evenodd",
-    clipRule: "evenodd"
-  }}
-  viewBox="0 0 784.11 815.53"
->
-  <g id="Layer_x0020_1">
-    <path
-      className="fil0"
-      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-    />
-  </g>
-</svg>
-
-  </div>
-  <div className="star-2">
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  style={{
-    shapeRendering: "geometricPrecision",
-    textRendering: "geometricPrecision",
-    imageRendering: "optimizeQuality",
-    fillRule: "evenodd",
-    clipRule: "evenodd"
-  }}
-  viewBox="0 0 784.11 815.53"
->
-  <g id="Layer_x0020_1">
-    <path
-      className="fil0"
-      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-    />
-  </g>
-</svg>
-
-  </div>
-  <div className="star-3">
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  style={{
-    shapeRendering: "geometricPrecision",
-    textRendering: "geometricPrecision",
-    imageRendering: "optimizeQuality",
-    fillRule: "evenodd",
-    clipRule: "evenodd"
-  }}
-  viewBox="0 0 784.11 815.53"
->
-  <g id="Layer_x0020_1">
-    <path
-      className="fil0"
-      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-    />
-  </g>
-</svg>
-
-  </div>
-  <div className="star-4">
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  style={{
-    shapeRendering: "geometricPrecision",
-    textRendering: "geometricPrecision",
-    imageRendering: "optimizeQuality",
-    fillRule: "evenodd",
-    clipRule: "evenodd"
-  }}
-  viewBox="0 0 784.11 815.53"
->
-  <g id="Layer_x0020_1">
-    <path
-      className="fil0"
-      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-    />
-  </g>
-</svg>
-
-  </div>
-  <div className="star-5">
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  style={{
-    shapeRendering: "geometricPrecision",
-    textRendering: "geometricPrecision",
-    imageRendering: "optimizeQuality",
-    fillRule: "evenodd",
-    clipRule: "evenodd"
-  }}
-  viewBox="0 0 784.11 815.53"
->
-  <g id="Layer_x0020_1">
-    <path
-      className="fil0"
-      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-    />
-  </g>
-</svg>
-
-  </div>
-  <div className="star-6">
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  version="1.1"
-  style={{
-    shapeRendering: "geometricPrecision",
-    textRendering: "geometricPrecision",
-    imageRendering: "optimizeQuality",
-    fillRule: "evenodd",
-    clipRule: "evenodd"
-  }}
-  viewBox="0 0 784.11 815.53"
->
-  <g id="Layer_x0020_1">
-    <path
-      className="fil0"
-      d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
-    />
-  </g>
-</svg>
-
-  </div>
-</button>
-</Link>
+          {userInfo.userType === "client" ? (
+            <button
+              className="button_home"
+              onClick={() =>
+                (window.location.href = "mailto:StoonProduction@gmail.com")
+              }
+            >
+              <h2
+                className="button_home_1"
+                style={
+                  !auth.isAdmin ? { color: "black" } : { color: "#0185B7" }
+                }
+              >
+                For Hosting
+              </h2>
+              <h2
+                className="button_home_2"
+                style={
+                  !auth.isAdmin
+                    ? { color: "#ffff", backgroundColor: "#5BD6FF" }
+                    : { color: "#ffff", backgroundColor: "#5BD6FF" }
+                }
+              >
+                Contact us
+              </h2>
+              {/* <img src="elements/arrow.svg" alt="" className="arrow"/> */}
+              <img
+                src={
+                  !auth.isAdmin ? "elements/arrow.svg" : "elements/arrow.svg"
+                }
+                alt=""
+                className="arrow"
+                style={{ padding: "9px 0", color: "black" }}
+              />
+            </button>
+          ) : (
+            <Link to="/Plans">
+              <button className="magic-button">
+                Get Your Plan
+                <div className="star-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                      shapeRendering: "geometricPrecision",
+                      textRendering: "geometricPrecision",
+                      imageRendering: "optimizeQuality",
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                  >
+                    <g id="Layer_x0020_1">
+                      <path
+                        className="fil0"
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+                <div className="star-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                      shapeRendering: "geometricPrecision",
+                      textRendering: "geometricPrecision",
+                      imageRendering: "optimizeQuality",
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                  >
+                    <g id="Layer_x0020_1">
+                      <path
+                        className="fil0"
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+                <div className="star-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                      shapeRendering: "geometricPrecision",
+                      textRendering: "geometricPrecision",
+                      imageRendering: "optimizeQuality",
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                  >
+                    <g id="Layer_x0020_1">
+                      <path
+                        className="fil0"
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+                <div className="star-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                      shapeRendering: "geometricPrecision",
+                      textRendering: "geometricPrecision",
+                      imageRendering: "optimizeQuality",
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                  >
+                    <g id="Layer_x0020_1">
+                      <path
+                        className="fil0"
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+                <div className="star-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                      shapeRendering: "geometricPrecision",
+                      textRendering: "geometricPrecision",
+                      imageRendering: "optimizeQuality",
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                  >
+                    <g id="Layer_x0020_1">
+                      <path
+                        className="fil0"
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+                <div className="star-6">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    style={{
+                      shapeRendering: "geometricPrecision",
+                      textRendering: "geometricPrecision",
+                      imageRendering: "optimizeQuality",
+                      fillRule: "evenodd",
+                      clipRule: "evenodd",
+                    }}
+                    viewBox="0 0 784.11 815.53"
+                  >
+                    <g id="Layer_x0020_1">
+                      <path
+                        className="fil0"
+                        d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+              </button>
+            </Link>
+          )}
           <div className="social-links2">
-          <Link to="#">  <img src={Facebook} alt="Facebook" /></Link>
-          <Link to="#">  <img src={Instagram} alt="Instagram" /></Link>
-          <Link to="#">  <img src={Twitter} alt="FaceTwitterbook" /></Link>
-          <Link to="#">  <img src={Youtube} alt="Youtube" /></Link>
-        </div>
+            <Link to="#">
+              {" "}
+              <img src={Facebook} alt="Facebook" />
+            </Link>
+            <Link to="#">
+              {" "}
+              <img src={Instagram} alt="Instagram" />
+            </Link>
+            <Link to="#">
+              {" "}
+              <img src={Twitter} alt="FaceTwitterbook" />
+            </Link>
+            <Link to="#">
+              {" "}
+              <img src={Youtube} alt="Youtube" />
+            </Link>
+          </div>
           <ul className="icons_home">
             <li>
               {" "}
@@ -224,10 +266,10 @@ const Introduction = () => {
               <img src="elements/home/Facebook.svg" alt="" />
             </li>
           </ul>
-         {/* <button className="LogoutBtn" onClick={logoutHandler}>Logout</button>*/}
+          {/* <button className="LogoutBtn" onClick={logoutHandler}>Logout</button>*/}
         </div>
       </div>
-     
+
       {/* {auth.isLoggedIn && <Hackatons />} */}
     </>
   );
