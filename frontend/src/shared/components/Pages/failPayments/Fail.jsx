6d8c8cf9ad548c56fe3eedccddb339cf78e940a2
Tarek <tarekchebbi33@gmail.com>
2024-05-15 11:02:14 +0100

    changing

--- frontend/src/shared/components/Pages/failPayments/Fail.jsx
+++ frontend/src/shared/components/Pages/failPayments/Fail.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import './Fail.css';
+import Faield from "../../../../assets/images/faield.svg";
+import { Link } from "react-router-dom";
 const Fail = () => {
   return (
-    <div>
-       <h1>Fail</h1>
+    <div id="Payment-Faield-section">
+    <div className="payment-failed-container-fail">
+    <div className="icon-fail">
+      <img src={Faield} alt='faield' />
     </div>
+    <h1>Payment failed</h1>
+    <p>Something went wrong we couldn’t process your payment, contact our support if you have lost money.</p>
+    <Link to="/home">
+    <button className="retry-button-fail" >Go Back HomePage</button>
+    </Link>
+  </div>
+  </div>
   );
 }
 
