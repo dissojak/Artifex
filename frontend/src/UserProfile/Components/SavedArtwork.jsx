4a215dc455d9e2bbe54ba58e8b8d7289ff48cb4f
Tarek <tarekchebbi33@gmail.com>
2024-05-25 09:35:00 +0100

    Tarek : pagination SavedArtwork

--- frontend/src/UserProfile/Components/SavedArtwork.jsx
+++ frontend/src/UserProfile/Components/SavedArtwork.jsx
@@ -2,46 +2,69 @@ import React, { useEffect, useState } from "react";
 import "./SavedArtwork.css";
 import Art from "../../assets/images/image_artwork.png";
 import loading from "../../assets/images/loadpurple.gif";
-
 import heart from "../../assets/images/heart.png";
 import eye from "../../assets/images/eye.png";
 import { useGetSavedArtworksMutation } from "../../slices/likeSaveSlice";
 import { toast } from "react-toastify";
 import ArtsList from "../../home/Components/ArtsList";
-const SavedArwork = () => {
-  const [artworks, setArtworks] = useState();
+
+const SavedArtwork = () => {
+  const [artworks, setArtworks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [artworksPerPage] = useState(6);
   const [getSavedArtworks, { isLoading }] = useGetSavedArtworksMutation();
+
   useEffect(() => {
-    const req = async () => {
+    const fetchArtworks = async () => {
       try {
         const responseData = await getSavedArtworks().unwrap();
-        // console.log("adem");
-        const populatedArtworks = responseData.savedArtworks.map(
-          (item) => item.artworkId
-        );
-        console.log(populatedArtworks);
+        const populatedArtworks = responseData.savedArtworks.map(item => item.artworkId);
         setArtworks(populatedArtworks);
       } catch (err) {
         toast.error(err?.data?.message || err.error);
       }
     };
-    req();
+    fetchArtworks();
   }, []);
+
+  // Get current artworks
+  const indexOfLastArt = currentPage * artworksPerPage;
+  const indexOfFirstArt = indexOfLastArt - artworksPerPage;
+  const currentArtworks = artworks.slice(indexOfFirstArt, indexOfLastArt);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  // Generate page numbers for pagination
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(artworks.length / artworksPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   return (
     <div className="savedartwork-container">
-      <div id="LikedArtworksContainerClinet">
-        {isLoading && (
-          <div className="center_spinner">
-            <img src={loading} alt="" />
+      {isLoading ? (
+        <div className="center_spinner">
+          <img src={loading} alt="Loading..." />
+        </div>
+      ) : (
+        <>
+          <ArtsList items={currentArtworks} />
+          <div className="pagination-container20">
+            <ul className="pagination20">
+              {pageNumbers.map(number => (
+                <li key={number} className="page-item20">
+                  <a onClick={() => paginate(number)}  className={`page-link20 ${currentPage === number ? 'active' : ''}`}>
+                    {number}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
-        )}
-        {/* <div className="likedContainer"> */}
-        {!isLoading && artworks && <ArtsList items={artworks} />}
-      </div>
-      {/* </div> */}
+        </>
+      )}
     </div>
   );
 };
 
-export default SavedArwork;
+export default SavedArtwork;
