509ddec8935e2eb47e9720f2756c2d34017597ed
Adem Ben Amor <dissojak@gmail.com>
2024-06-01 23:02:09 +0100

    adem protect routes by userType

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,31 +3,22 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  useLocation,
+  // useLocation,
   // Redirect,
   //  Switch,
 } from "react-router-dom";
 
-import Home from "./home/Page/HomePage.jsx";
-import Ban from "./shared/components/Pages/Ban";
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces.jsx";
-import UpdatePlace from "./places/pages/UpdatePlace.jsx";
-import Auth from "./user/Auth/Auth.jsx";
-import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
-import { AuthContext } from "./shared/context/auth-context";
-import Signup from "./user/pages/Signup.jsx";
-import RegisterHackatons from "./register/Page/RegisterHackatons.jsx";
-import Profile from "./profile/Pages/Profile.jsx";
-
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import store from "./store";
 import RegisterScreen from "./screens/RegisterScreen";
-import ProfileScreen from "./screens/ProfileScreen";
 import PrivateRoute from "./BuildKite/PrivateRoute.jsx";
+
+import Auth from "./user/Auth/Auth.jsx";
+import Home from "./home/Page/HomePage.jsx";
+import Ban from "./shared/components/Pages/Ban";
+import UserPlaces from "./places/pages/UserPlaces.jsx";
 
 import Landing from "./landing/pages/Landing.jsx";
 import NewArtwork from "./shared/components/FormElements/NewArtworkSignup.jsx";
@@ -55,147 +46,129 @@ import LikedArtworks from "./LikedArtworks/pages/LikedArtworks.jsx";
 import SuccessOrderPayment from "./shared/components/Pages/successPayments/SuccessOrderPayment.jsx";
 import FailOrderPayment from "./shared/components/Pages/failPayments/FailOrderPayment.jsx";
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userId, setUserId] = useState();
-  const [userName, setUserName] = useState();
-  const [ban, setBan] = useState();
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [teamId, setTeamId] = useState();
-  const [profileImage, setProfileImage] = useState();
-
-  const login = useCallback(
-    (userId, userName, teamId, isAdmin = false, ban = false) => {
-      setIsLoggedIn(true);
-      setUserId(userId);
-      setUserName(userName);
-      setIsAdmin(isAdmin);
-      setTeamId(teamId);
-      setBan(ban);
-    },
-    []
+// Helper component to handle protected routes by userType
+const ProtectedRoute = ({ children, userType, allowedTypes }) => {
+  const { userInfo } = useSelector((state) => state.auth);
+  return allowedTypes.includes(userInfo.userType) ? (
+    children
+  ) : (
+    <Navigate to="/" />
   );
+};
 
-  const updateUsername = useCallback((userName) => {
-    setUserName(userName);
-  }, []);
-
-  const updateProfilePicture = useCallback((URLimage) => {
-    setProfileImage(URLimage);
-  }, []);
-
-  const logout = useCallback(() => {
-    setIsLoggedIn(false);
-    setUserId(null);
-    setUserName(null);
-    setIsAdmin(false);
-    setTeamId(null);
-    setBan(null);
-  }, []);
-
-  // const location = useLocation();
-  // const publicPaths = ["/login", ];
-
-  // // Check if the current path is one of the protected paths
-  // const showMainNavigation = publicPaths.includes(location.pathname);
-
-  let routes;
-
-  if (isLoggedIn && !isAdmin && !ban) {
-    routes = (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:userId/places" element={<UserPlaces />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/places/:placeId" element={<UpdatePlace />} />
-        <Route path="/registred-hackatons" element={<RegisterHackatons />} />
-        <Route path="/users" element={<Users />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    );
-  } else if (isLoggedIn && !isAdmin && ban) {
-    routes = (
-      <Routes>
-        <Route path="/" element={<Ban />} />
-      </Routes>
-    );
-  } else if (isLoggedIn && isAdmin) {
-    routes = (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    );
-  } else {
-    routes = (
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="" element={<PrivateRoute />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/artist" element={<Artists />} />
-          <Route path="/museums" element={<Museums />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/artwork/:artworkId" element={<ArtworkDetails />} />
-          <Route path="/ArtistProfileAdem" element={<ArtistProfileClient />} />
-          <Route path="/artist/:username" element={<ArtistProfileForUser />} />
-          <Route path="/museumshowcase" element={<MuseumShowcase />} />
-          <Route
-            path="/MuseumShowcaseArtist"
-            element={<MuseumShowcaseArtist />}
-          />
-          <Route path="/Plans" element={<PricingOptions />} />
-          <Route path="/PlansHistory" element={<PlansHistory />} />
-          <Route path="/museums/:museumId" element={<MuseumDetails />} />
-          <Route path="/collection" element={<Collection />} />
-          <Route path="/artwork/success" element={<Success />} />
-          <Route path="/artwork/fail" element={<Fail />} />
-          <Route path="/museum/success" element={<SuccessPaymentMuseum />} />
-          <Route path="/museum/fail" element={<FailPaymentMuseum />} />
-          <Route path="/Card" element={<Card />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/artworks/liked" element={<LikedArtworks />} />
-          <Route path="/order/success" element={<SuccessOrderPayment />} />
-          <Route path="/order/fail" element={<FailOrderPayment />} />
-        </Route>
-        <Route path="/login" element={<Auth />} />
-        {/* <Route path="/auth" element={<Auth />} /> */}
-        <Route path="/:userId/places" element={<UserPlaces />} />
-        <Route path="/signup" element={<Auth signup={true} />} />
-        <Route path="/register" element={<RegisterScreen />} />
-        <Route path="/AddArtwork" element={<NewArtwork />} />
-        <Route path="/socialMedia" element={<SocialMedia />} />
-      </Routes>
-    );
-  }
+const App = () => {
+  let routes = (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute allowedTypes={["client"]}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/artist"
+          element={
+            <ProtectedRoute allowedTypes={["client"]}>
+              <Artists />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/museums" element={<Museums />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/artwork/:artworkId" element={<ArtworkDetails />} />
+        <Route
+          path="/artist/:username"
+          element={
+            <ProtectedRoute allowedTypes={["client"]}>
+              <ArtistProfileForUser />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/museumshowcase" element={<MuseumShowcase />} />
+        <Route
+          path="/MuseumShowcaseArtist"
+          element={<MuseumShowcaseArtist />}
+        />
+        <Route
+          path="/Plans"
+          element={
+            <ProtectedRoute allowedTypes={["artist"]}>
+              <PricingOptions />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/PlansHistory"
+          element={
+            <ProtectedRoute allowedTypes={["artist"]}>
+              <PlansHistory />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/museums/:museumId" element={<MuseumDetails />} />
+        <Route
+          path="/collection"
+          element={
+            <ProtectedRoute allowedTypes={["client"]}>
+              <Collection />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/artwork/success" element={<Success />} />
+        <Route path="/artwork/fail" element={<Fail />} />
+        <Route path="/museum/success" element={<SuccessPaymentMuseum />} />
+        <Route path="/museum/fail" element={<FailPaymentMuseum />} />
+        <Route
+          path="/Card"
+          element={
+            <ProtectedRoute allowedTypes={["client"]}>
+              <Card />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/events"
+          element={
+            <ProtectedRoute allowedTypes={["client", "artist"]}>
+              <Events />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/artworks/liked"
+          element={
+            <ProtectedRoute allowedTypes={["client"]}>
+              <LikedArtworks />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/order/success" element={<SuccessOrderPayment />} />
+        <Route path="/order/fail" element={<FailOrderPayment />} />
+      </Route>
+      <Route path="/login" element={<Auth />} />
+      {/* <Route path="/auth" element={<Auth />} /> */}
+      <Route path="/:userId/places" element={<UserPlaces />} />
+      <Route path="/signup" element={<Auth signup={true} />} />
+      <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/AddArtwork" element={<NewArtwork />} />
+      <Route path="/socialMedia" element={<SocialMedia />} />
+    </Routes>
+  );
 
   return (
     <Provider store={store}>
-      <AuthContext.Provider
-        value={{
-          isLoggedIn: isLoggedIn,
-          userId: userId,
-          userName: userName,
-          ban: ban,
-          teamId: teamId,
-          isAdmin: isAdmin,
-          profileImage: profileImage,
-          login: login,
-          logout: logout,
-          updateUsername: updateUsername,
-          updateProfilePicture: updateProfilePicture,
-        }}
-      >
-        <Router>
-          {/* <Header /> */}
-          <ToastContainer style={{ marginTop: "80px" }} />
-          <NavigationWrapper>
-            {/*this is sent as a children to navigation 
+      <Router>
+        <ToastContainer style={{ marginTop: "80px" }} />
+        <NavigationWrapper>
+          {/*this is sent as a children to navigation 
             warpper that will handel rundering it */}
-            {routes}
-          </NavigationWrapper>
-        </Router>
-      </AuthContext.Provider>
+          {routes}
+        </NavigationWrapper>
+      </Router>
     </Provider>
   );
 };
---
+++ frontend/src/shared/components/Pages/NoAuthorization/NoAuthorization.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoAuthorization = () => {
+  return (
+    <div>
+       <h1>NoAuthorization!!!</h1>
+       <h2>403</h2>
+    </div>
+  );
+}
+
+export default NoAuthorization;
