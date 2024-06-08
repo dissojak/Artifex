a41acec9f49cd77d8f96f8887b46d4a86e9669ad
Adem Ben Amor <dissojak@gmail.com>
2024-06-08 20:23:59 +0100

    adem : follow button artists

--- frontend/src/ArtistsPage/Components/ArtistCardItem.jsx
+++ frontend/src/ArtistsPage/Components/ArtistCardItem.jsx
@@ -5,6 +5,7 @@ import "./SkeletonLoader.css";
 import Diamant from "../../assets/images/platinumBadge2.svg";
 import Platinum from "../../assets/images/diamant.svg";
 import Gold from "../../assets/images/crown.svg";
+import Unfollow from "../../assets/images/unfollowIcon.svg";
 
 // import { useGetUserMutation } from "../../slices/usersApiSlice";
 
@@ -145,28 +146,40 @@ const ArtistCardItem = (props) => {
 
         <div className="follow_social9">
           <button className="bookmarkBtn9">
-            <span className="IconContainer9">
+            <span
+              className={
+                !props.isFollowing ? "IconContainer9" : "IconContainer9_hovered"
+              }
+            >
               {/* Insert the SVG directly here */}
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon"
-                style={{ width: "1.5em", height: "1.5em" }}
-              >
-                <g>
-                  <path
-                    fill="#ffffff"
-                    d="M13 17.62a5.29 5.29 0 0 0 .38 2l.06.14H3.5a2.25 2.25 0 0 1-2.25-2.26V17A6.71 6.71 0 0 1 4 11.55a6.7 6.7 0 0 0 8.92 0 6.7 6.7 0 0 1 1.87 2.06A5.24 5.24 0 0 0 13 17.62z"
-                  />
-                  <circle fill="#ffffff" r="5.25" cy="6.5" cx="8.5" />
-                  <path
-                    fill="#ffffff"
-                    d="M18.38 13.25A4.37 4.37 0 0 0 14 17.62a4.53 4.53 0 0 0 .3 1.62 4.38 4.38 0 1 0 4.08-6zm1.84 5.07h-1.15v1.15a.7.7 0 0 1-1.39 0v-1.15h-1.15a.7.7 0 0 1 0-1.39h1.15v-1.15a.7.7 0 0 1 1.39 0v1.15h1.15a.7.7 0 0 1 0 1.39z"
-                  />
-                </g>
-              </svg>
+              {!props.isFollowing ? (
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon"
+                  style={{ width: "1.5em", height: "1.5em" }}
+                >
+                  <g>
+                    <path
+                      fill="#ffffff"
+                      d="M13 17.62a5.29 5.29 0 0 0 .38 2l.06.14H3.5a2.25 2.25 0 0 1-2.25-2.26V17A6.71 6.71 0 0 1 4 11.55a6.7 6.7 0 0 0 8.92 0 6.7 6.7 0 0 1 1.87 2.06A5.24 5.24 0 0 0 13 17.62z"
+                    />
+                    <circle fill="#ffffff" r="5.25" cy="6.5" cx="8.5" />
+                    <path
+                      fill="#ffffff"
+                      d="M18.38 13.25A4.37 4.37 0 0 0 14 17.62a4.53 4.53 0 0 0 .3 1.62 4.38 4.38 0 1 0 4.08-6zm1.84 5.07h-1.15v1.15a.7.7 0 0 1-1.39 0v-1.15h-1.15a.7.7 0 0 1 0-1.39h1.15v-1.15a.7.7 0 0 1 1.39 0v1.15h1.15a.7.7 0 0 1 0 1.39z"
+                    />
+                  </g>
+                </svg>
+              ) : (
+                <img
+                  src={Unfollow}
+                  style={{ width: "16px"}}
+                  alt="unfollow_icon"
+                />
+              )}
             </span>
-            <p className="text9">follow</p>
+            {!props.isFollowing ? <p className="text9">follow</p> : null}
           </button>
 
           {/* Example for a single social icon, repeat structure for others */}
